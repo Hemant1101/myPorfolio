7ded04a08fc6aa693e14ed891e77547b38936b38
hemant1101 <hemantmandal1101@gmail.com>
2023-02-13 21:48:43 +0530

    sidebar done

--- myPortfolio/src/components/MobileMenu.jsx
+++ myPortfolio/src/components/MobileMenu.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../utilities/Button'
+import { menuItems } from './MenuList'
 
 const MobileMenu = () => {
+  const [isOpen,setisOpen] = useState(false)
+
+  function handleHamClick(){
+    
+    if(!isOpen){
+      if(typeof window != 'undefined' && window.document){ 
+        document.body.classList.add('blur')
+      }
+    }
+    if(isOpen){
+      document.body.classList.remove('blur')
+    }
+    setisOpen(!isOpen)
+  }
+
   return (
-    <div>MobileMenu</div>
+    <div id="mobilemenu" >
+      <button className={`mobile-menu-button ${isOpen ? "mobile-open" : "mobile-closed" }`} onClick={handleHamClick}>
+        <div className="ham-box">
+          <div className="ham-box-inner">
+          </div>
+        </div>
+      </button>
+      <aside aria-hidden={`${isOpen ? "false" : "true" }`} tabIndex={`${isOpen ? "1" : "-1" }`} className={`mobile-menu-container ${isOpen ? "aside-open" : "aside-closed" }`}>
+        <nav>
+          <ol>
+            {menuItems.map((values)=>{
+              return (
+                <li key={values.id}>
+                  <a href={values.href}>
+                    {`${values.name}`}
+                  </a>
+                </li>
+              )
+            })
+            }
+          </ol>
+          <Button href="/resume.pdf" classname="resume-link" target="_blank">Resume</Button>
+        </nav>
+      </aside>
+    </div>
   )
 }
 
